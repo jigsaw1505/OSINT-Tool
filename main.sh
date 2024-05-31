44d83b26fcc9c854089147643b658b18e527f47e
Jigar R Rathod <136077334+jrrathod1505@users.noreply.github.com>
2024-06-01 00:20:48 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,29 @@
-
+function display(){
+echo "Choose your option"
+echo"1. Username search"
+echo"2. Domain search"
+echo"3. Mail search"
+echo"4. Exit"
+}
+while true do
+display
+read -r choice
+case $choise in
+1)
+    bash src/uname.sh
+    ;;
+2)
+    bash src/domain.sh
+    ;;
+3) 
+    bash src/email.sh
+    ;;
+4)
+    exit
+    ;;
+*)
+    echo"enter valid option"
+    esac
+    done
+    
+    
