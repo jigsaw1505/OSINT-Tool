fb678d099531a0429b893c9798b798858e2d5308
Jigar R Rathod <136077334+jrrathod1505@users.noreply.github.com>
2024-06-03 14:07:28 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,35 +1,37 @@
-function display(){
-echo "Choose your option"
-echo"1. Username search"
-echo"2. Domain search"
-echo"3. Mail search"
-echo"4. Exit"
+#!/bin/bash
+
+function display() {
+    echo "Choose your option"
+    echo "1. Username search"
+    echo "2. Domain search"
+    echo "3. Email search"
+    echo "4. Exit"
 }
-while true do
-display
-read -r choice
-case $choise in
-1)
-    echo"Enter username"
-    read -r uname
-    bash src/uname.sh $uname
-    ;;
-2)
-    echo"Enter domain name"
-    read -r domain
-    bash src/domain.sh $domain
-    ;;
-3) 
-    echo "Enter email address"
-    read -r email
-    bash src/email.sh $email 
-    ;;
-4)
-    exit
-    ;;
-*)
-    echo"enter valid option"
+
+while true; do
+    display
+    read -r choice
+    case $choice in
+        1)
+            echo "Enter username:"
+            read -r uname
+            bash src/uname.sh "$uname"
+            ;;
+        2)
+            echo "Enter domain name:"
+            read -r domain
+            bash src/domain.sh "$domain"
+            ;;
+        3)
+            echo "Enter email address:"
+            read -r email
+            bash src/email.sh "$email"
+            ;;
+        4)
+            exit
+            ;;
+        *)
+            echo "Enter a valid option"
+            ;;
     esac
 done
-    
-    
